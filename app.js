0fb05921db619ae25e384bd6c0088edf837fbe61
Jonathan Smiley <92873891+jonnysmiley@users.noreply.github.com>
2021-12-14 13:22:14 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+let weather = {
+  apiKey: "61d9fbb6355ac18b3426ee7c4e151a8a",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=imperial&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity, temp_max, temp_min } = data.main;
+    const { speed } = data.wind;
+
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = Math.floor(temp) + "°F";
+    document.querySelector(".temp_max").innerText = "Temp-Max: " + Math.floor(temp_max) + "°F";
+    document.querySelector(".temp_min").innerText = "Temp-Min: " + Math.floor(temp_min) + "°F";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
